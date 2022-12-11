af62811f4485ad6c34a068a2a8d7778a7689f6ec
the80hz <dnl.vlgv@mail.ru>
2022-12-11 19:13:04 +0400

    1

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* The text is given. Convert it as follows:
- * Remove all repetitions of the word except the first occurrence, for all words that occur more than once in this text.
+ * Remove all repetitions of the word except the first, for all words that occur more than once in this text.
  * The first word should be marked with brackets
  */
 
@@ -26,13 +26,40 @@ char *get_word(char *str, char *word) {
     return str;
 }
 
-int main() {
-    char text[MAX_LEN];
+// Function that converts all letters to lowercase
+void to_lower(char *str) {
+    while (*str) {
+        *str = tolower(*str);
+        str++;
+    }
+}
 
-    printf("Enter text: ");
+int main() {
+    // dictionary of punctuation marks
+    const char *punctuation_marks = " .,!?;:-<>[]{}()";
+
+    // input string
+    char text[MAX_LEN];
     fgets(text, MAX_LEN, stdin);
 
+    // get words from text
+    char word[MAX_LEN];
+    char *str = text;
+    char temp[MAX_LEN];
+
+    while (*str) {
+        str = get_word(str, word);
+        if (strchr(punctuation_marks, word[strlen(word) - 1])) {
+            word[strlen(word) - 1] = '\0';
+        }
+        to_lower(word);
+        if (strlen(word) > 0) {
+            strcat(temp, word);
+            strcat(temp, " ");
+        }
+    }
 
 
     return 0;
 }
+
