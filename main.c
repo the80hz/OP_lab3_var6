2af388503a23ca07227ee942ed6d41b484f59a9c
the80hz <dnl.vlgv@mail.ru>
2022-12-12 20:46:00 +0400

    переделал логику словаря, но все еще только с англ символами

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define SENTENCE 1000
 #define WORD 50
 
-char *get_word(char *str, char *word) {
+char* get_word(char* str, char* word) {
     int i = 0;
     while (isspace(*str))
         str++;
@@ -16,7 +16,7 @@ char *get_word(char *str, char *word) {
     return str;
 }
 
-void to_lower(char *str) {
+void to_lower(char* str) {
     while (*str) {
         *str = tolower(*str);
         str++;
@@ -24,14 +24,14 @@ void to_lower(char *str) {
 }
 
 int main() {
-    const char *punctuation_marks = " .,!?;:-<>[]{}()";
-    char dictionary[SENTENCE][SENTENCE];
+    const char* punctuation_marks = " .,!?;:-<>[]{}()";
+    char dictionary[SENTENCE] = "";
     char result[SENTENCE] = "";
 
     char text[SENTENCE];
     fgets(text, SENTENCE, stdin);
 
-    char *main = text;
+    char* main = text;
     char word1[WORD];
     char word1_clean[WORD];
 
@@ -46,49 +46,52 @@ int main() {
 
         if (strlen(word1_clean) > 0) {
             to_lower(word1_clean);
-            char *main2 = main;
+            char* main2 = main;
             char word2[WORD];
             char word2_clean[WORD];
 
             // проверка на будущее совпадение
-            while(*main2){
+            while (*main2) {
                 main2 = get_word(main2, word2);
                 strcpy(word2_clean, word2);
                 strcat(word2, " ");
-                if(strchr(punctuation_marks, word2_clean[strlen(word2_clean) - 1]))
+                if (strchr(punctuation_marks, word2_clean[strlen(word2_clean) - 1]))
                     word2_clean[strlen(word2_clean) - 1] = '\0';
-                if(strlen(word2_clean) > 0) {
+                if (strlen(word2_clean) > 0) {
                     to_lower(word2_clean);
-                    if(strcmp(word1_clean, word2_clean) == 0)
+                    if (strcmp(word1_clean, word2_clean) == 0)
                         flag_is_next = 1;
                 }
             }
 
-            // проверка на наличие в словаре
-            for(int i = 0; i < SENTENCE; i++)
-                if(strcmp(dictionary[i], word1_clean) == 0)
-                    flag_is_in_dictionary = 1;
+			// проверка на наличие в словаре
+			char* main3 = dictionary;
+			char word3[WORD];
+			while (*main3) {
+				main3 = get_word(main3, word3);
+				if (strcmp(word1_clean, word3) == 0)
+					flag_is_in_dictionary = 1;
+			}
 
-            if(flag_is_in_dictionary == 0){
-                for(int i = 0; i < SENTENCE; i++)
-                    if(strlen(dictionary[i]) == 0) {
-                        strcpy(dictionary[i], word1_clean);
-                        break;
-                    }
+			// добавление очищенного слова в словарь если его там нет
+            if (flag_is_in_dictionary == 0) {
+				strcat(dictionary, word1_clean);
+				strcat(dictionary, " ");
             }
         }
 
-        if(flag_is_next == 1 && flag_is_in_dictionary == 0){
+        // Если слово повторяется в будущем и не повторялось в прошлом, то в скобки
+        if (flag_is_next == 1 && flag_is_in_dictionary == 0) {
             strcat(result, "(");
             strcat(result, word1);
             result[strlen(result) - 1] = '\0';
             strcat(result, ") ");
         }
-        if(flag_is_next == 0 && flag_is_in_dictionary == 0)
+        // Если слово не повторяется в будущем и не повторялось в прошлом, то просто записать
+        if (flag_is_next == 0 && flag_is_in_dictionary == 0)
             strcat(result, word1);
     }
 
     printf("%s", result);
     return 0;
 }
-
